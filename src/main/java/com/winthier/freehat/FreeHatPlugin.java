972291b1b55319f4407d7ffef427e4757ab28ba0
StarTux <ths.loy@googlemail.com>
2021-11-25 13:37:06 +0100

    Mytems support

--- src/main/java/com/winthier/freehat/FreeHatPlugin.java
+++ src/main/java/com/winthier/freehat/FreeHatPlugin.java
@@ -1,13 +1,14 @@
 package com.winthier.freehat;
 
+import com.cavetale.mytems.Mytems;
 import com.destroystokyo.paper.MaterialTags;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -28,51 +29,41 @@ public final class FreeHatPlugin extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command command,
                              String string, String[] args) {
-        if (!(sender instanceof HumanEntity)) {
-            msg(sender, "&cPlayer expected");
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("[freehat:hat] Player expected");
             return true;
         }
         if (args.length != 0) return false;
         Player player = (Player) sender;
         PlayerInventory playerInventory = player.getInventory();
-        ItemStack old = playerInventory.getHelmet();
-        if (old != null && old.getType() != Material.AIR) {
-            msg(player, "&cYou are already wearing a hat.");
+        ItemStack oldHelmet = playerInventory.getHelmet();
+        if (oldHelmet != null && oldHelmet.getType() != Material.AIR) {
+            player.sendMessage(Component.text("You are already wearing a hat", NamedTextColor.RED));
             return true;
         }
         ItemStack hand = playerInventory.getItemInMainHand();
         if (hand == null || hand.getType() == Material.AIR) {
-            msg(player, "&cThere's nothing in your hand.");
+            player.sendMessage(Component.text("There's nothing in your hand", NamedTextColor.RED));
             return true;
         }
-        if (hand.getAmount() > 1) {
-            msg(player, "&cYou cannot wear " + hand.getAmount()
-                + " items on your head.");
-            return true;
-        }
-        playerInventory.setHelmet(hand);
-        playerInventory.setItemInMainHand(old);
-        msg(player, "&bEnjoy your fancy %s hat!", niceItemName(hand));
+        ItemStack helmet = hand.clone();
+        helmet.setAmount(1);
+        hand.subtract(1);
+        playerInventory.setHelmet(helmet);
+        player.sendMessage(Component.text("Enjoy your fancy ", NamedTextColor.GREEN)
+                           .append(niceItemName(helmet))
+                           .append(Component.text(" hat!")));
         return true;
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
     void onInventoryClick(InventoryClickEvent event) {
-        if (event.getClick() != ClickType.LEFT) {
-            return;
-        }
-        if (event.getInventory().getType() != InventoryType.CRAFTING) {
-            return;
-        }
-        if (event.getSlotType() != InventoryType.SlotType.ARMOR) {
-            return;
-        }
-        if (event.getSlot() != 39) {
-            return;
-        }
-        if (!(event.getWhoClicked() instanceof Player)) {
-            return;
-        }
+        if (event.getClick() != ClickType.LEFT) return;
+        if (event.getInventory().getType() != InventoryType.CRAFTING) return;
+        if (event.getSlotType() != InventoryType.SlotType.ARMOR) return;
+        if (event.getSlot() != 39) return;
+        if (event.getClickedInventory() == null) return;
+        if (!(event.getWhoClicked() instanceof Player)) return;
         Player player = (Player) event.getWhoClicked();
         if (!player.hasPermission("freehat.click")) {
             return;
@@ -82,26 +73,33 @@ public final class FreeHatPlugin extends JavaPlugin implements Listener {
             return;
         }
         if (MaterialTags.HEAD_EQUIPPABLE.isTagged(cursor.getType())) return;
-        if (cursor.getAmount() > 1) return;
-        event.setCancelled(true);
-        // Update
-        ItemStack old = event.getCurrentItem();
-        player.getInventory().setHelmet(cursor);
-        event.getView().setCursor(old);
-        msg(player, "&bEnjoy your fancy %s hat!", niceItemName(cursor));
-    }
-
-    static void msg(CommandSender sender, String msg, Object... args) {
-        msg = ChatColor.translateAlternateColorCodes('&', msg);
-        if (args.length > 0) {
-            msg = String.format(msg, args);
+        ItemStack oldHelmet = event.getCurrentItem();
+        boolean hasOldHat = oldHelmet != null && oldHelmet.getType() != Material.AIR;
+        if (hasOldHat && cursor.getAmount() > 1) {
+            return;
         }
-        sender.sendMessage(msg);
+        // Update
+        event.setCancelled(true);
+        ItemStack helmet = cursor.clone();
+        helmet.setAmount(1);
+        event.setCurrentItem(helmet);
+        if (hasOldHat) {
+            event.getView().setCursor(oldHelmet);
+        } else {
+            cursor.subtract(1);
+        }
+        player.sendActionBar(Component.text("Enjoy your fancy ", NamedTextColor.GREEN)
+                             .append(niceItemName(helmet))
+                             .append(Component.text(" hat!")));
     }
 
-    static String niceItemName(ItemStack item) {
-        return Stream.of(item.getType().name().split("_"))
-            .map(s -> s.substring(0, 1) + s.substring(1).toLowerCase())
-            .collect(Collectors.joining(" "));
+    public static Component niceItemName(ItemStack item) {
+        Mytems mytems = Mytems.forItem(item);
+        if (mytems != null) return mytems.getMytem().getDisplayName();
+        String i18n = item.getI18NDisplayName();
+        if (i18n != null) return Component.text(i18n);
+        return Component.text(Stream.of(item.getType().name().split("_"))
+                              .map(s -> s.substring(0, 1) + s.substring(1).toLowerCase())
+                              .collect(Collectors.joining(" ")));
     }
 }
